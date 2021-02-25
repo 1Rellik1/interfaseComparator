912c5b8f273f737804a427485066a708baf4c53e
Rellik <Alexzz123@yandex.ru>
2021-02-25 15:27:42 +0300

    4 redone

--- src/main/java/Homework/fourth/Smth.java
+++ src/main/java/Homework/fourth/Smth.java
@@ -1,73 +1,25 @@
 package Homework.fourth;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Smth implements ExecutorService {
-
-    @Override
-    public void shutdown() {
-
+public class Smth  {
+    private int number_of_Threads;
+    private ExecutorService service;
+    public Smth(int n){
+        number_of_Threads=n;
+        service=Executors.newFixedThreadPool(number_of_Threads);
     }
-
-    @Override
-    public List<Runnable> shutdownNow() {
-        return null;
-    }
-
-    @Override
-    public boolean isShutdown() {
-        return false;
-    }
-
-    @Override
-    public boolean isTerminated() {
-        return false;
-    }
-
-    @Override
-    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
-        return false;
-    }
-
-    @Override
-    public <T> Future<T> submit(Callable<T> task) {
-        return null;
-    }
-
-    @Override
-    public <T> Future<T> submit(Runnable task, T result) {
-        return null;
-    }
-
-    @Override
-    public Future<?> submit(Runnable task) {
-        return null;
-    }
-
-    @Override
-    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
-        return null;
-    }
-
-    @Override
-    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
-        return null;
-    }
-
-    @Override
-    public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
-        return null;
-    }
-
-    @Override
-    public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-        return null;
-    }
-
-    @Override
-    public void execute(Runnable command) {
-
+    public void dosmth() {
+        for (int i = 0; i < number_of_Threads; i++) {
+                int finalI = i;
+                service.submit(new Runnable() {
+                    public void run() {
+                        Thread.currentThread().setName(Integer.toString(finalI));
+                        System.out.println(Thread.currentThread().getName());
+                    }
+            });
+        }
+    service.shutdown();
     }
 }
