2fc40816921fe105c1a5fcbec8c2123852650fbc
Rellik <Alexzz123@yandex.ru>
2021-03-16 12:35:54 +0300

    8

--- src/main/java/Homework/main.java
+++ src/main/java/Homework/main.java
@@ -1,4 +1,12 @@
 package Homework;
 
+import Homework.eighth.Template_Method.Facebook;
+import Homework.eighth.Template_Method.Network;
+
 public class main {
+    public static void main(String[] args) {
+        Facebook facebook = new Facebook("Rellik", "123321");
+        Boolean logedin;
+        logedin = facebook.logIn("Rellik", "123321");
+    }
 }
